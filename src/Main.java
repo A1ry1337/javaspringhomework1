17d3447b53bff006d2cd5d5fb24728d98b4d3c1b
A1ry1337 <mixail-aristov07072003@mail.ru>
2024-09-15 01:25:53 +0500

    задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
     /**
-     * Задача:
+     * Задача 1:
      * Найти сумму всех чисел от 0 до 1000, которые делятся на 3 или 5.
      * Если число кратно 3 или 5, оно добавляется к итоговой сумме.
      * Функция возвращает эту сумму.
@@ -14,7 +14,29 @@ public class Main {
         return sum;
     }
 
+    /**
+     * Задача 2:
+     * Найти минимальный элемент в двумерном массиве целых чисел.
+     * Функция принимает двумерный массив и возвращает наименьшее значение в этом массиве.
+     */
+    public static int matrix_min_el(int[][] matrix) {
+        int min = matrix[0][0];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] < min) min = matrix[i][j];
+            }
+        }
+        return min;
+    }
+
     public static void main(String[] args) {
-        System.out.println(sum_x3_x5());
+        System.out.println(sum_x3_x5());  // 1 задача
+
+        int[][] matrix = {  // 2 задача
+                {20, 34, 2},
+                {9, 12, 18},
+                {3, 4, 5}
+        };
+        System.out.println(matrix_min_el(matrix));
     }
 }
