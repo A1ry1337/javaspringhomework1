9864465c197af21519163a65a12c32d0eeb89659
A1ry1337 <mixail-aristov07072003@mail.ru>
2024-09-15 00:49:40 +0500

    задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,20 @@
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+    /**
+     * Задача:
+     * Найти сумму всех чисел от 0 до 1000, которые делятся на 3 или 5.
+     * Если число кратно 3 или 5, оно добавляется к итоговой сумме.
+     * Функция возвращает эту сумму.
+     */
+    public static int sum_x3_x5() {
+        int sum = 0;
+        for (int i = 0; i < 1001; i++) {
+            if (i % 3 == 0 || i % 5 == 0) sum += i;
         }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum_x3_x5());
     }
 }
